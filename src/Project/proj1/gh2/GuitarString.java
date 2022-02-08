9ed75aa2a4ed5cdcbf2929b9599df8716d2ef17f
FelixLin99 <604462291@qq.com>
2022-02-08 22:46:40 +0800

    update_20220208

--- src/Project/proj1/gh2/GuitarString.java
+++ src/Project/proj1/gh2/GuitarString.java
@@ -1,7 +1,7 @@
 package Project.proj1.gh2;
 
 // TODO: uncomment the following import once you're ready to start this portion
-// import deque.Deque;
+import Project.proj1.deque.Deque;
 // TODO: maybe more imports
 
 //Note: This file will not compile until you complete the Deque implementations
@@ -14,7 +14,7 @@ public class GuitarString {
 
     /* Buffer for storing sound data. */
     // TODO: uncomment the following line once you're ready to start this portion
-    // private Deque<Double> buffer;
+    private Deque<Double> buffer;
 
     /* Create a guitar string of the given frequency.  */
     public GuitarString(double frequency) {
@@ -22,6 +22,7 @@ public class GuitarString {
         //       cast the result of this division operation into an int. For
         //       better accuracy, use the Math.round() function before casting.
         //       Your should initially fill your buffer array with zeros.
+        buffer.addFirst(1.2);
     }
 
 
